1e0dccc1760dbddbd553e8b99b9b4080ecb65a7a
sceiler <yimin.yang0+github@gmail.com>
2026-03-18 11:13:23 +0100

    Update product-header.tsx

--- examples/shirt-shop/components/product-detail-page/product-header.tsx
+++ examples/shirt-shop/components/product-detail-page/product-header.tsx
@@ -4,8 +4,8 @@ export function ProductHeader() {
   return (
     <div className="lg:col-span-5 lg:col-start-8">
       <div className="flex justify-between">
-        <h1 className="text-xl font-medium text-gray-900">Circles T-Shirt</h1>
-        <p className="text-xl font-medium text-gray-900">1.00 USD</p>
+        <h1 className="text-xl font-medium text-gray-900">Circles T-Shirt Deluxe</h1>
+        <p className="text-xl font-medium text-gray-900">100.00 USD</p>
       </div>
       <ProductReviews />
     </div>
